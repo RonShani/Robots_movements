7a8a30ba9a42192efea2f535be3500c816eedbe7
RonShani <ronicube@gmail.com>
2023-08-21 20:26:05 +0300

    all dependencies

---
+++ .pio/libdeps/esp01/Regressino/PolynomialRegression.h
@@ -0,0 +1,20 @@
+#ifndef PolynomialRegression_h
+#define PolynomialRegression_h
+
+#include "Arduino.h"
+#include "LinearRegression2d.h"
+
+class PolynomialRegression {
+    public:
+        void learn(double x, double y);
+        double calculate(double x);
+        double correlation();
+        void parameters(double values[]);
+        void reset();
+        int samples();
+        double error(double x, double y); 
+    private:
+        LinearRegression2d lr2d;
+};
+
+#endif
